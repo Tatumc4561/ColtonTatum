9fd5a3ca3b0e69871a75f66c15b384f93d3b1b12
Tatumc4561 <tatumc4561@gmail.com>
2022-06-20 22:43:03 -0700

    database

---
+++ portfolio_proj/settings/development.py
@@ -0,0 +1,33 @@
+from portfolio_proj.settings.common import *
+import dotenv
+
+
+DEBUG = True
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["NAME"],
+        "USER": os.environ["USER"],
+        "PASSWORD": os.environ["PASSWORD"],
+        "HOST": os.environ["HOST"],
+        "PORT": os.environ["PORT"],
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
---
+++ portfolio_proj/settings/production.py
@@ -0,0 +1,21 @@
+from portfolio_proj.settings.common import *
+
+
+DEBUG = False
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = ["colton-portfolio.herokuapp.com"]
+
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# dropbox
+# DEFAULT_FILE_STORAGE = "storages.backends.dropbox.DropBoxStorage"
+
+
+# DBX_TOKEN = os.environ["DBX_TOKEN"]
+# DROPBOX_OAUTH2_TOKEN = DBX_TOKEN
+
+# DROPBOX_ROOT_PATH = "/media/"
