39eecce193385c4ebb15c5f0c429c79d6d0ead23
Tatumc4561 <tatumc4561@gmail.com>
2022-06-23 16:31:12 -0700

    d

--- portfolio_proj/urls.py
+++ portfolio_proj/urls.py
@@ -16,14 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("portfolio_app.urls")),
 ]
-
-urlpatterns += static(
-    settings.production.MEDIA_URL, document_root=settings.production.MEDIA_ROOT
-)
