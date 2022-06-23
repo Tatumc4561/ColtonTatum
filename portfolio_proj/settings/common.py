1e2e3b1803950da6b9797c5b8da77082074281f8
Tatumc4561 <tatumc4561@gmail.com>
2022-06-23 16:12:28 -0700

    d

--- portfolio_proj/settings/common.py
+++ portfolio_proj/settings/common.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "portfolio_app",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -42,6 +43,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "portfolio_proj.urls"
