558a9478bad8ec15159d4b8439cc00965fc8be58
Tatumc4561 <tatumc4561@gmail.com>
2022-06-24 16:56:07 -0700

    static

---
+++ staticfiles/js/app.js
@@ -0,0 +1,7 @@
+const projectMenu = document.querySelector('.arrow-container')
+const projectView = document.querySelector('#sectiontwo')
+
+projectMenu.addEventListener('click', function(e){
+    projectView.classList.toggle('sectionTwoslide')
+    console.log('bitch')
+})
